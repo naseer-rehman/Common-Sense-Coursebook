a86cc9c3358eb244fb5712d33be92fd67bc71ae9
nacereal <n5rehman@uwaterloo.ca>
2022-01-15 01:01:36 -0500

    Setup base files

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Header.css"
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header">
+        
+      </div>
+    );
+  }
+}
+
+export default Header;
