a3301c44b7f7339f1f9f2f5009a55ecfc5269d11
nacereal <n5rehman@uwaterloo.ca>
2022-01-15 18:11:41 -0500

    Ayaz: Add theme button | change header style

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>Built and designed by CSC</p>
+    <p>All rights reserved. ©</p>
+  </div>
+);
+
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,43 @@
 import React from "react";
 import "./Header.css"
-import logo from '../images/darkmode.svg';
+// import lightmode from "../images/lightmode.svg";
+import DarkMode from "./DarkMode.js";
+
 
 class Header extends React.Component {
+
+
+
   render() {
+
     return (
-      <div className="header">
-        <a className="header-title"><span>C</span>ommon <span>S</span>ense <span>C</span>oursebook</a>
-        
+      /*<div className="header">
+        <div className="header-left">
+          <a className="header-title"><span>C</span>ommon <span>S</span>ense <span>C</span>oursebook</a>
+        </div>
+
         <div className="header-right">
-          <a onClick="darkMode()">
-          <img src={logo} className="App-logo" alt="logo" /></a>
+          <DarkMode />
           <a href="#help" className="help-button"><span>Help</span></a>
         </div>
 
+      </div>*/
+
+      <div className="main">
+        <div className="left">
+          <a className="header-title"><span>C</span>ommon <span>S</span>ense <span>C</span>oursebook</a>
+        </div>
+
+        <div className="right">
+          <div className="main space-between">
+            <div>
+              <DarkMode />
+              <button class="help">Help</button>
+            </div>
+          </div>
+        </div>
       </div>
+
     );
   }
 }
